c5655e9f4e46b677f855d3f36858e5973412d3f4
Julien Collet <julien.collet@cern.ch>
2018-02-20 10:14:00 +0100

    librbd: add image access/last modified timestamps
    Add access and last_modified timestamps to RBD images.
    Access (resp. last_modified) timestamps are updated on
    read (resp. write) operations performed 60s after the
    last update.
    
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

--- src/librbd/image/OpenRequest.cc
+++ src/librbd/image/OpenRequest.cc
@@ -358,6 +358,138 @@ Context *OpenRequest<I>::handle_v2_get_create_timestamp(int *result) {
     return nullptr;
   }
 
+  send_v2_get_access_timestamp();
+  return nullptr;
+}
+
+template<typename I>
+ void OpenRequest<I>::set_access_timestamp() {
+   
+   librados::ObjectWriteOperation op;
+   cls_client::set_access_timestamp(&op, ceph_clock_now());
+ 
+   using klass = OpenRequest<I>;
+   librados::AioCompletion *comp =
+     create_rados_callback<klass, &klass::handle_set_access_timestamp>(this);
+   m_image_ctx->md_ctx.aio_operate(m_image_ctx->header_oid, comp, &op);
+   comp->release();
+ }
+ 
+ template<typename I>
+ void OpenRequest<I>::handle_set_access_timestamp(int r) {
+   CephContext *cct = m_image_ctx->cct;
+   ldout(cct, 10) << this << " " << __func__ << ": r=" << r << dendl;
+   if (r < 0) {
+     lderr(cct) << "error setting access_timestamp: "
+                  << cpp_strerror(r) << dendl;
+     return;
+   }
+
+  send_v2_get_access_timestamp();
+}
+
+template<typename I>
+ void OpenRequest<I>::set_modified_timestamp() {
+   
+   librados::ObjectWriteOperation op;
+   cls_client::set_modified_timestamp(&op, ceph_clock_now());
+ 
+   using klass = OpenRequest<I>;
+   librados::AioCompletion *comp =
+     create_rados_callback<klass, &klass::handle_set_modified_timestamp>(this);
+   m_image_ctx->md_ctx.aio_operate(m_image_ctx->header_oid, comp, &op);
+   comp->release();
+ }
+ 
+ template<typename I>
+ void OpenRequest<I>::handle_set_modified_timestamp(int r) {
+   CephContext *cct = m_image_ctx->cct;
+   ldout(cct, 10) << this << " " << __func__ << ": r=" << r << dendl;
+   if (r < 0) {
+     lderr(cct) << "error setting modified_timestamp: "
+                  << cpp_strerror(r) << dendl;
+     return;
+   }
+
+  send_v2_get_access_timestamp();
+}
+
+
+template <typename I>
+void OpenRequest<I>::send_v2_get_access_timestamp() {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << this << " " << __func__ << dendl;
+
+  librados::ObjectReadOperation op;
+  cls_client::get_access_timestamp_start(&op);
+
+  using klass = OpenRequest<I>;
+  librados::AioCompletion *comp = create_rados_callback<
+    klass, &klass::handle_v2_get_access_timestamp>(this);
+  m_out_bl.clear();
+  m_image_ctx->md_ctx.aio_operate(m_image_ctx->header_oid, comp, &op,
+                                  &m_out_bl);
+  comp->release();
+}
+
+template <typename I>
+Context *OpenRequest<I>::handle_v2_get_access_timestamp(int *result) {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << this << " " << __func__ << ": r=" << *result << dendl;
+
+  if (*result == 0) {
+    bufferlist::iterator it = m_out_bl.begin();
+    *result = cls_client::get_access_timestamp_finish(&it,
+        &m_image_ctx->access_timestamp);
+  }
+  if (*result < 0 && *result != -EOPNOTSUPP) {
+    lderr(cct) << "failed to retrieve access_timestamp: "
+               << cpp_strerror(*result)
+               << dendl;
+    send_close_image(*result);
+    return nullptr;
+  }
+
+  send_v2_get_modified_timestamp();
+  return nullptr;
+}
+
+
+template <typename I>
+void OpenRequest<I>::send_v2_get_modified_timestamp() {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << this << " " << __func__ << dendl;
+
+  librados::ObjectReadOperation op;
+  cls_client::get_modified_timestamp_start(&op);
+
+  using klass = OpenRequest<I>;
+  librados::AioCompletion *comp = create_rados_callback<
+    klass, &klass::handle_v2_get_modified_timestamp>(this);
+  m_out_bl.clear();
+  m_image_ctx->md_ctx.aio_operate(m_image_ctx->header_oid, comp, &op,
+                                  &m_out_bl);
+  comp->release();
+}
+
+template <typename I>
+Context *OpenRequest<I>::handle_v2_get_modified_timestamp(int *result) {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << this << " " << __func__ << ": r=" << *result << dendl;
+
+  if (*result == 0) {
+    bufferlist::iterator it = m_out_bl.begin();
+    *result = cls_client::get_modified_timestamp_finish(&it,
+        &m_image_ctx->modified_timestamp);
+  }
+  if (*result < 0 && *result != -EOPNOTSUPP) {
+    lderr(cct) << "failed to retrieve modified_timestamp: "
+               << cpp_strerror(*result)
+               << dendl;
+    send_close_image(*result);
+    return nullptr;
+  }
+
   send_v2_get_data_pool();
   return nullptr;
 }
